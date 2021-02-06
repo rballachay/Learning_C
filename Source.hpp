68108621efedf027edf488dcaa841cc9b5fe56c9
Riley Ballachay <riley.ballachay@gmail.com>
2021-02-06 14:59:52 -0700

    First Commit

---
+++ Source.hpp
@@ -0,0 +1,13 @@
+//
+//  Source.hpp
+//  Mandlebrot_Set
+//
+//  Created by Riley Ballachay on 2021-02-06.
+//
+
+#ifndef Source_hpp
+#define Source_hpp
+
+#include <stdio.h>
+
+#endif /* Source_hpp */
